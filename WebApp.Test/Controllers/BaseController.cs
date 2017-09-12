1ea9b4e90b56a559987a55f3a010d1c76b56946a
NguyenQuynh <quynhnv@novaon.vn>
2017-09-12 16:58:10 +0700

    code

---
+++ WebApp.Test/Controllers/BaseController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp.Test.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected IConfiguration _baseConfig;
+
+        protected async System.Threading.Tasks.Task<T> GetAsync<T>(string uri)
+        {
+            string responseJson = string.Empty;
+            using (var client = new HttpClient())
+            {
+                var baseUri = "http://localhost:39048/api/";
+                client.BaseAddress = new System.Uri(baseUri);
+                client.DefaultRequestHeaders.Accept.Clear();
+                var response = await client.GetAsync(baseUri + uri);
+                if (response.IsSuccessStatusCode)
+                {
+                    responseJson = await response.Content.ReadAsStringAsync();
+                }
+            }
+            return Newtonsoft.Json.JsonConvert.DeserializeObject<T>(responseJson);
+        }
+
+        protected HttpResponseMessage Get(string uri)
+        {
+            string responseJson = string.Empty;
+            using (var client = new HttpClient())
+            {
+                var baseUri = "http://localhost:39048/api/";
+                client.BaseAddress = new System.Uri(baseUri);
+                client.DefaultRequestHeaders.Accept.Clear();
+                var response = client.GetAsync(baseUri + uri);
+                return response.Result;
+            }
+        }
+
+        protected async System.Threading.Tasks.Task<string> PostAsync(string uri, StringContent data)
+        {
+            string responseJson = string.Empty;
+            using (var client = new HttpClient())
+            {
+                var baseUri = "http://localhost:39048/api/";
+                client.BaseAddress = new System.Uri(baseUri);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Add("UserName", GetCurrentUserName());
+
+                var response = await client.PostAsync(baseUri + uri, data);
+                if (response.IsSuccessStatusCode)
+                {
+                    responseJson = await response.Content.ReadAsStringAsync();
+                }
+            }
+
+            return Newtonsoft.Json.JsonConvert.DeserializeObject<string>(responseJson);
+        }
+
+        protected HttpResponseMessage Post(string uri, StringContent data)
+        {
+            string responseJson = string.Empty;
+            using (var client = new HttpClient())
+            {
+                var baseUri = _baseConfig["ApiUrl"];
+                client.BaseAddress = new System.Uri(baseUri);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.Add("UserName", GetCurrentUserName());
+
+                var response = client.PostAsync(baseUri + uri, data);
+                return response.Result;
+            }
+        }
+
+        
+
+        protected string GetCurrentUserName()
+        {
+            
+            return string.Empty;
+        }
+    }
+}
---
+++ WebApp.Test/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp.Test.Controllers
+{
+    public class HomeController : BaseController
+    {
+        
+        public async Task<ActionResult> Index()
+        {
+            string user = "quynhnv";
+            string pass = "quynhvan@123";
+            var token= "";
+            var baseUri = "http://localhost:39048/api/";
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new System.Uri(baseUri);
+                client.DefaultRequestHeaders.Accept.Clear();
+                var response = await client.GetAsync(baseUri + "token/get?username=" + user + "&password=" + pass);
+                if (response.IsSuccessStatusCode)
+                {
+                    token = await response.Content.ReadAsStringAsync();                    
+                }
+            }
+            token = Newtonsoft.Json.JsonConvert.DeserializeObject<string>(token);
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new System.Uri(baseUri);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+                var response = await client.GetAsync(baseUri + "Value/Get?s=quynhnv111");
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseJson = await response.Content.ReadAsStringAsync();
+                }
+            }
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
